148ecfd8b0ed252918de540c754f8b00a0a6ee17
azolayza <azovcevae@gmail.com>
2020-10-03 01:19:40 +0700

    new varian of task

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 public class MainClass
     {
@@ -6,14 +7,7 @@ public class MainClass
        @Test
          public void testGetClassNumber() {
             int c = getClassNumber();
-            if (c>45) {
-                System.out.print("Test pass: c more than 45");
-            } else
-                if (c==0) {
-                System.out.println("Test failed: c is zero");
-            } else {
-                    System.out.print("Test failed: c less than 45");
-           }
+           Assert.assertTrue("GetClassNumber less than 45", c>45);
        }
 
         public int getClassNumber() {
